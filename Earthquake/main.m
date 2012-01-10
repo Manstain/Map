a93a2d310b814db50e7512d5337d0d6fdbb33552
Pavel <pavel@Mac-mini-developer.local>
2012-01-10 18:36:07 +0200

    Initial Commit

---
+++ Earthquake/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Earthquake
+//
+//  Created by Pavel on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "myAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([myAppDelegate class]));
+    }
+}
---
+++ Earthquake/myViewController.h
@@ -0,0 +1,13 @@
+//
+//  myViewController.h
+//  Earthquake
+//
+//  Created by Pavel on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myViewController : UIViewController
+
+@end
